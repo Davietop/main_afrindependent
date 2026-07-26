21c04b6543973c217c14ae0c2ed9b9ad1840d7fb
David <oyatoyedavid@gmail.com>
2026-07-26 04:21:33 -0500

    fixing deployment issue again

--- app/about/historyFounder.tsx
+++ app/about/historyFounder.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import mission_img from "../../public/AfridependentLogo.svg";
 import manuel from "../../public/manuel_copy.png";
 import { IBM_Plex_Sans } from "next/font/google";
 const ibmPlexSans = IBM_Plex_Sans({
