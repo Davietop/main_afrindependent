f83f3667d2f3c8e1d3352684815857c4a702af7e
David <oyatoyedavid@gmail.com>
2026-07-26 04:25:23 -0500

    fixing deployment issue

--- app/about/historyFounder.tsx
+++ app/about/historyFounder.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import manuel from "../../public/manuel_copy.png";
-import { IBM_Plex_Sans } from "next/font/google";
+import manuel from "/public/manuel_copy.png";
+import { IBM_Plex_Sans } from "next/font/google"; 
 const ibmPlexSans = IBM_Plex_Sans({
   subsets: ["latin"], 
   weight: ["400", "500", "700"], 
